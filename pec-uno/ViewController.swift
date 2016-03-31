6f80e11cad81e1fc2d19de3babeed4820ec3d0d7
Julian Gernun <julian.gernun@gmail.com>
2016-03-31 21:25:40 +0200

    Initial Commit

---
+++ pec-uno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pec-uno
+//
+//  Created by Julian Gernun on 31/3/16.
+//  Copyright © 2016 uoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
