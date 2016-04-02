97b7da8e33a131d18aff9a80dd42f938198a6e42
julian-uoc <jgernun@uoc.edu>
2016-04-02 12:37:05 +0200

    added next button through createAccount form

--- pec-uno/CreateAccountViewController.swift
+++ pec-uno/CreateAccountViewController.swift
@@ -1,9 +1,58 @@
 //
-//  CreateAccountViewController.swift
+//  LoginViewController.swift
 //  pec-uno
 //
-//  Created by Julian Gernun on 1/4/16.
+//  Created by Julian Gernun on 31/3/16.
 //  Copyright © 2016 uoc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CreateAccountViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pwd: UITextField!
+    @IBOutlet weak var pwdRepeat: UITextField!
+    @IBOutlet weak var register: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initDelegates()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initDelegates() {
+        username.delegate = self
+        fullName.delegate = self
+        email.delegate = self
+        pwd.delegate = self
+        pwdRepeat.delegate = self
+    }
+
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("OMG")
+        
+        switch textField {
+        case username: fullName.becomeFirstResponder()
+        case fullName: email.becomeFirstResponder()
+        case email: pwd.becomeFirstResponder()
+        case pwd: pwdRepeat.becomeFirstResponder()
+        default: textField.resignFirstResponder()
+        }
+        
+        return false
+        
+    }
+}
