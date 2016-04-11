63a3d242fac59883298ef1d9aacf39ffb68bcf33
jcger <jgernun@uoc.edu>
2016-04-11 23:09:22 +0200

    terms as html file

---
+++ pec-uno/TermsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TermsViewController.swift
+//  pec-uno
+//
+//  Created by Julian Gernun on 11/4/16.
+//  Copyright © 2016 uoc. All rights reserved.
+//
+
+class TermsViewController: UIViewController {
+    
+    @IBOutlet weak var termsWebView: UIWebView!
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadTermsHTML()
+    }
+    
+    
+    func loadTermsHTML() {
+        
+        //Load the HTML file from resources
+        guard let path = NSBundle.mainBundle()
+            .pathForResource("terms", ofType: "html") else {
+                return
+        }
+        
+        let url = NSURL(fileURLWithPath: path)
+        
+        if let data = NSData(contentsOfURL: url) {
+            
+            termsWebView.loadHTMLString(NSString(data: data,
+                encoding: NSUTF8StringEncoding) as! String, baseURL: nil)
+            
+        }
+    }
+}
